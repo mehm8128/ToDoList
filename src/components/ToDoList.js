98bba6f8d338e2b67ff6eaebbc580a88974d42b3
mehm8128 <mehm8128@gmail.com>
2022-02-20 14:55:33 +0900

    fix for day-1

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,8 +1,6 @@
 import { useState } from "react"
-import "./ToDoList.css"
 
 function ToDoList() {
-	const [completedTodos, setCompletedTodos] = useState([])
 	const [todos, setTodos] = useState([])
 	const [todo, setTodo] = useState("")
 
@@ -12,35 +10,15 @@ function ToDoList() {
 			setTodo("")
 		}
 	}
-	function handleComplete(index) {
-		setCompletedTodos(completedTodos.concat(todos[index]))
-		setTodos(todos.slice(0, index).concat(todos.slice(index + 1)))
-	}
 	return (
-		<div className="todoList">
-			<h1 className="title">ToDoリスト</h1>
+		<div>
+			<h1>ToDoリスト</h1>
 			<input value={todo} onChange={(e) => setTodo(e.target.value)} />
-			<button onClick={handlePush} className="button">
-				登録
-			</button>
+			<button onClick={handlePush}>登録</button>
 			<h2>タスク一覧</h2>
-			<h3>完</h3>
-			<ul className="ul">
-				{completedTodos.map((todo, index) => (
-					<li key={index} className="li">
-						{todo}
-					</li>
-				))}
-			</ul>
-			<h3>未完</h3>
-			<ul className="ul">
+			<ul>
 				{todos.map((todo, index) => (
-					<li key={index} className="li">
-						{todo}
-						<button onClick={() => handleComplete(index)} className="button">
-							完了する
-						</button>
-					</li>
+					<li key={index}>{todo}</li>
 				))}
 			</ul>
 		</div>
