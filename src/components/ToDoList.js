6e437832617db1a3319772cd2370a95b3eebd727
mehm8128 <mehm8128@gmail.com>
2022-02-21 15:34:26 +0900

    day-3

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import axios from "axios"
+import { useState, useEffect } from "react"
 import "./ToDoList.css"
 
 function ToDoList() {
@@ -8,14 +9,49 @@ function ToDoList() {
 
 	function handlePush() {
 		if (todo !== "") {
-			setTodos(todos.concat([todo]))
+			axios
+				.post("http://localhost:8000/api/tasks", {
+					name: todo,
+				})
+				.then(() => {
+					handleGetTodos()
+				})
 			setTodo("")
 		}
 	}
 	function handleComplete(index) {
-		setCompletedTodos(completedTodos.concat(todos[index]))
-		setTodos(todos.slice(0, index).concat(todos.slice(index + 1)))
+		axios.put(`http://localhost:8000/api/tasks/` + todos[index].id).then(() => {
+			handleGetTodos()
+		})
 	}
+	function handleDelete(index) {
+		axios
+			.delete(`http://localhost:8000/api/tasks/` + todos[index].id)
+			.then(() => {
+				handleGetTodos()
+			})
+	}
+	function handleGetTodos() {
+		axios.get("http://localhost:8000/api/tasks").then((res) => {
+			for (let i = 0; i < res.data.length; i++) {
+				if (res.data[i].finished) {
+					setCompletedTodos({
+						id: completedTodos.concat([res.data[i].id]),
+						name: completedTodos.concat([res.data[i].name]),
+					})
+				} else {
+					setTodos({
+						id: todos.concat([res.data[i].id]),
+						name: todos.concat([res.data[i].name]),
+					})
+				}
+			}
+		})
+	}
+	useEffect(() => {
+		handleGetTodos()
+	}, []) // eslint-disable-line react-hooks/exhaustive-deps
+
 	return (
 		<div className="todoList">
 			<h1 className="title">ToDoリスト</h1>
@@ -25,24 +61,32 @@ function ToDoList() {
 			</button>
 			<h2>タスク一覧</h2>
 			<h3>完</h3>
-			<ul className="ul">
-				{completedTodos.map((todo, index) => (
-					<li key={index} className="li">
-						{todo}
-					</li>
-				))}
-			</ul>
+			{completedTodos.length ? (
+				<ul className="ul">
+					{completedTodos.map((completedTodo, index) => (
+						<li key={index} className="li">
+							{completedTodo.name}
+							<button onClick={() => handleDelete(index)} className="button">
+								タスクを消す
+							</button>
+						</li>
+					))}
+				</ul>
+			) : null}
+
 			<h3>未完</h3>
-			<ul className="ul">
-				{todos.map((todo, index) => (
-					<li key={index} className="li">
-						{todo}
-						<button onClick={() => handleComplete(index)} className="button">
-							完了する
-						</button>
-					</li>
-				))}
-			</ul>
+			{todos.length ? (
+				<ul className="ul">
+					{todos.map((todo, index) => (
+						<li key={index} className="li">
+							{todo.name}
+							<button onClick={() => handleComplete(index)} className="button">
+								完了する
+							</button>
+						</li>
+					))}
+				</ul>
+			) : null}
 		</div>
 	)
 }
